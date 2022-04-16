476d513f0f83be1db9119fa5d6189828e3c18740
Abhigya Sodani <abhigyasodani@gmail.com>
2022-04-16 12:01:52 -0700

    nice

--- air_travel_index.py
+++ air_travel_index.py
@@ -1,5 +1,6 @@
 import pandas
 import pickle
+import numpy as np
 """
 ati_dict={}
 with open('Airports2.csv', newline='') as csvfile:
@@ -18,6 +19,66 @@ f=open("ati_dict.pkl","wb")
 pickle.dump(ati_dict,f)
 f.close()
 """
+us_state = {
+    "Alabama": "AL",
+    "Alaska": "AK",
+    "Arizona": "AZ",
+    "Arkansas": "AR",
+    "California": "CA",
+    "Colorado": "CO",
+    "Connecticut": "CT",
+    "Delaware": "DE",
+    "Florida": "FL",
+    "Georgia": "GA",
+    "Hawaii": "HI",
+    "Idaho": "ID",
+    "Illinois": "IL",
+    "Indiana": "IN",
+    "Iowa": "IA",
+    "Kansas": "KS",
+    "Kentucky": "KY",
+    "Louisiana": "LA",
+    "Maine": "ME",
+    "Maryland": "MD",
+    "Massachusetts": "MA",
+    "Michigan": "MI",
+    "Minnesota": "MN",
+    "Mississippi": "MS",
+    "Missouri": "MO",
+    "Montana": "MT",
+    "Nebraska": "NE",
+    "Nevada": "NV",
+    "New Hampshire": "NH",
+    "New Jersey": "NJ",
+    "New Mexico": "NM",
+    "New York": "NY",
+    "North Carolina": "NC",
+    "North Dakota": "ND",
+    "Ohio": "OH",
+    "Oklahoma": "OK",
+    "Oregon": "OR",
+    "Pennsylvania": "PA",
+    "Rhode Island": "RI",
+    "South Carolina": "SC",
+    "South Dakota": "SD",
+    "Tennessee": "TN",
+    "Texas": "TX",
+    "Utah": "UT",
+    "Vermont": "VT",
+    "Virginia": "VA",
+    "Washington": "WA",
+    "West Virginia": "WV",
+    "Wisconsin": "WI",
+    "Wyoming": "WY",
+    "District of Columbia": "DC",
+    "American Samoa": "AS",
+    "Guam": "GU",
+    "Northern Mariana Islands": "MP",
+    "Puerto Rico": "PR",
+    "United States Minor Outlying Islands": "UM",
+    "U.S. Virgin Islands": "VI",
+}
+
 car_pickle = open ("ati_dict.pkl", "rb")
 x = pickle.load(car_pickle)
 x={k: v for k, v in sorted(x.items(), key=lambda item: item[1],reverse=True)}
@@ -35,5 +96,23 @@ for i in x.items():
       norm_factor=i[1]
 for i in x.items():
    x[i[0]]/=norm_factor
-print(internal_flying)
+#print(internal_flying)
+#print(x)
 print(x)
+states=list(us_state.values())
+a = np.zeros((50,50,2))
+
+counter1=0
+counter2=0
+
+while counter2<50:
+   counter1=0
+   while counter1<50:
+      if(states[counter1]+"-"+states[counter2] in x.keys()):
+         
+         a[counter2][counter1][1]=x[states[counter1]+"-"+states[counter2]]
+      counter1+=1
+         
+   counter2+=1
+
+print(a[states.index("FL")])
