6a4b54a1bd2e53528c231bb7dae128e18813aff3
Abhigya Sodani <abhigyasodani@gmail.com>
2022-04-16 09:19:32 -0700

    stuff

--- air_travel_index.py
+++ air_travel_index.py
@@ -1,5 +1,39 @@
-import csv
+import pandas
+import pickle
+"""
+ati_dict={}
 with open('Airports2.csv', newline='') as csvfile:
-     airports = csv.reader(csvfile, delimiter=' ', quotechar='|')
-     for row in airports:
-       print(', '.join(row))
+     airports = pandas.read_csv("Airports2.csv")
+     
+     for i in airports.iloc:
+       origins=[str(j).replace(" ","") for j in i[2].split(",")]
+       destinations=[str(j).replace(" ","") for j in i[3].split(",")]
+       print(origins,destinations)
+       if(origins[1]+"-"+destinations[1] not in ati_dict.keys()):
+          ati_dict.update({origins[1]+"-"+destinations[1]:int(i[4])})
+       else:
+           ati_dict[origins[1]+"-"+destinations[1]]+=int(i[4])
+print(ati_dict)
+f=open("ati_dict.pkl","wb")
+pickle.dump(ati_dict,f)
+f.close()
+"""
+car_pickle = open ("ati_dict.pkl", "rb")
+x = pickle.load(car_pickle)
+x={k: v for k, v in sorted(x.items(), key=lambda item: item[1],reverse=True)}
+internal_flying={}
+remove_list=[]
+for i in x.items():
+   if(i[0].split("-")[0]==i[0].split("-")[1]):
+      internal_flying.update({i[0].split("-")[0]:int(i[1])})
+      remove_list.append(i[0])
+for j in remove_list:
+   del x[j]
+norm_factor=0
+for i in x.items():
+   if(i[1]>norm_factor):
+      norm_factor=i[1]
+for i in x.items():
+   x[i[0]]/=norm_factor
+print(internal_flying)
+print(x)
