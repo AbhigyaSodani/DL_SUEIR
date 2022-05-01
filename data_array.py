ca7437b76bde423f08bb70feb0325dccb9e327cb
Abhigya Sodani <abhigyasodani@gmail.com>
2022-04-30 17:31:50 -0700

    new

--- data_array.py
+++ data_array.py
@@ -61,69 +61,61 @@ us_state = {
     "United States Minor Outlying Islands": "UM",
     "U.S. Virgin Islands": "VI",
 }
-"""
-neighboring_dict=
-{
-    "Alabama":	"Mississippi, Tennessee, Florida, Georgia",
-2.	"Alaska":	None,	
-3.	"Arizona"	:Nevada, New Mexico, Utah, California, Colorado	
-4.	Arkansas	Oklahoma, Tennessee, Texas, Louisiana, Mississippi, Missouri	
-5.	California	Oregon, Arizona, Nevada
-3
-6.	Colorado	New Mexico, Oklahoma, Utah, Wyoming, Arizona, Kansas, Nebraska	
-7.	Connecticut	New York, Rhode Island, Massachusetts	
-8.	Delaware	New Jersey, Pennsylvania, Maryland	
-9.	Florida	Georgia, Alabama
-2
-10.	Georgia	North Carolina, South Carolina, Tennessee, Alabama, Florida	
-11.	Hawaii	None	
-12.	Idaho	Utah, Washington, Wyoming, Montana, Nevada, Oregon	
-13.	Illinois	Kentucky, Missouri, Wisconsin, Indiana, Iowa, Michigan	
-14.	Indiana	Michigan, Ohio, Illinois, Kentucky	
-15.	Iowa	Nebraska, South Dakota, Wisconsin, Illinois, Minnesota, Missouri	
-16.	Kansas	Nebraska, Oklahoma, Colorado, Missouri	
-17.	Kentucky	Tennessee, Virginia, West Virginia, Illinois, Indiana, Missouri, Ohio	
-18.	Louisiana	Texas, Arkansas, Mississippi	
-19.	Maine
-(The state which borders only one other U.S. state.)	New Hampshire
-1
-20.	Maryland	Virginia, West Virginia, Delaware, Pennsylvania	
-21.	Massachusetts	New York, Rhode Island, Vermont, Connecticut, New Hampshire	5
-22.	Michigan	Ohio, Wisconsin, Illinois, Indiana, Minnesota (water border)	5
-23.	Minnesota	North Dakota, South Dakota, Wisconsin, Iowa, Michigan (water border)	5
-24.	Mississippi	Louisiana, Tennessee, Alabama, Arkansas	4
-25.	Missouri
-(The state which touches the most other states.)	Nebraska, Oklahoma, Tennessee, Arkansas, Illinois, Iowa, Kansas, Kentucky	8
-26.	Montana	South Dakota, Wyoming, Idaho, North Dakota	4
-27.	Nebraska	Missouri, South Dakota, Wyoming, Colorado, Iowa, Kansas,	6
-28.	Nevada	Idaho, Oregon, Utah, Arizona, California	5
-29.	New Hampshire	Vermont, Maine, Massachusetts
-3
-30.	New Jersey	Pennsylvania, Delaware, New York	3
-31.	New Mexico	Oklahoma, Texas, Utah, Arizona, Colorado	5
-32.	New York	Pennsylvania, Rhode Island (water border), Vermont, Connecticut, Massachusetts, New Jersey	6
-33.	North Carolina	Tennessee, Virginia, Georgia, South Carolina	4
-34.	North Dakota	South Dakota, Minnesota, Montana	3
-35.	Ohio	Michigan, Pennsylvania, West Virginia, Indiana, Kentucky	5
-36.	Oklahoma	Missouri, New Mexico, Texas, Arkansas, Colorado, Kansas	6
-37.	Oregon	Nevada, Washington, California, Idaho	4
-38.	Pennsylvania	New York, Ohio, West Virginia, Delaware, Maryland, New Jersey	6
-39.	Rhode Island	Massachusetts, New York (water border), Connecticut
-3
-40.	South Carolina	North Carolina, Georgia,	2
-41.	South Dakota	Nebraska, North Dakota, Wyoming, Iowa, Minnesota, Montana	6
-42.	Tennessee
-(The state which touches the most other states.)	Mississippi, Missouri, North Carolina, Virginia, Alabama, Arkansas, Georgia, Kentucky	8
-43.	Texas	New Mexico, Oklahoma, Arkansas, Louisiana	4
-44.	Utah	Nevada, New Mexico, Wyoming, Arizona, Colorado, Idaho	6
-45.	Vermont	New Hampshire, New York, Massachusetts	3
-46.	Virginia	North Carolina, Tennessee, West Virginia, Kentucky, Maryland	5
-47.	Washington	Oregon, Idaho	2
-48.	West Virginia	Pennsylvania, Virginia, Kentucky, Maryland, Ohio	5
-49.	Wisconsin	Michigan, Minnesota, Illinois, Iowa	4
-50.	Wyoming	Nebraska, South Dakota, Utah, Colorado, Idaho, Montana
+
+
+neighboring_dict={
+    "Alabama":	"Mississippi,Tennessee,Florida,Georgia",
+	"Alaska":	None,	
+   "Arizona": "Nevada,New Mexico,Utah,California,Colorado"	,
+	"Arkansas":	"Oklahoma,Tennessee,Texas,Louisiana,Mississippi,Missouri"	,
+	"California":	"Oregon,Arizona,Nevada",
+   "Colorado":	"New Mexico,Oklahoma,Utah,Wyoming,Arizona,Kansas,Nebraska",	
+	"Connecticut":	"New York,Rhode Island,Massachusetts",	
+	"Delaware":	"New Jersey,Pennsylvania,Maryland",	
+	"Florida":	"Georgia,Alabama",
+   "Georgia":	"North Carolina,South Carolina,Tennessee,Alabama,Florida",	
+	"Hawaii":	None,	
+	"Idaho":	"Utah,Washington,Wyoming,Montana,Nevada,Oregon",	
+	"Illinois":	"Kentucky,Missouri,Wisconsin,Indiana,Iowa,Michigan",
+	"Indiana":	"Michigan,Ohio,Illinois,Kentucky",	
+	"Iowa":	"Nebraska,South Dakota,Wisconsin,Illinois,Minnesota,Missouri",	
+	"Kansas":"Nebraska,Oklahoma,Colorado,Missouri",	
+	"Kentucky":	"Tennessee,Virginia,West Virginia,Illinois,Indiana,Missouri,Ohio",	
+   "Louisiana":	"Texas,Arkansas,Mississippi",	
+	"Maine":	"New Hampshire",
+	"Maryland":	"Virginia,West Virginia,Delaware,Pennsylvania",	
+   "Massachusetts":	"New York,Rhode Island,Vermont,Connecticut,New Hampshire",	
+	"Michigan":	"Ohio,Wisconsin,Illinois,Indiana,Minnesota", 
+	"Minnesota": "North Dakota,South Dakota,Wisconsin,Iowa,Michigan", 
+	"Mississippi":	"Louisiana,Tennessee,Alabama,Arkansas",
+	"Missouri": "Nebraska,Oklahoma,Tennessee,Arkansas,Illinois,Iowa,Kansas,Kentucky",	
+	"Montana":	"South Dakota,Wyoming,Idaho,North Dakota",	
+	"Nebraska":	"Missouri,South Dakota,Wyoming,Colorado,Iowa,Kansas",
+	"Nevada": "Idaho,Oregon,Utah,Arizona,California",	
+	"New Hampshire": "Vermont,Maine,Massachusetts",
+	"New Jersey":	"Pennsylvania,Delaware,New York",	
+	"New Mexico":	"Oklahoma,Texas,Utah,Arizona,Colorado",	
+	"New York":	"Pennsylvania,Rhode Island,Vermont,Connecticut,Massachusetts,New Jersey",	
+	"North Carolina":	"Tennessee,Virginia,Georgia,South Carolina",	
+	"North Dakota":	"South Dakota,Minnesota,Montana",	
+   "Ohio":	"Michigan,Pennsylvania,West Virginia,Indiana,Kentucky",	
+	"Oklahoma":	"Missouri,New Mexico,Texas,Arkansas,Colorado,Kansas",	
+	"Oregon":	"Nevada,Washington,California,Idaho",	
+	"Pennsylvania":	"New York,Ohio,West Virginia,Delaware,Maryland,New Jersey",	
+	"Rhode Island":	"Massachusetts,New York,Connecticut",
+	"South Carolina":	"North Carolina,Georgia",	
+	"South Dakota":	"Nebraska,North Dakota,Wyoming,Iowa,Minnesota,Montana",
+	"Tennessee": "Mississippi,Missouri,North Carolina,Virginia,Alabama,Arkansas,Georgia,Kentucky",	
+	"Texas":	"New Mexico,Oklahoma,Arkansas,Louisiana",	
+	"Utah":	"Nevada,New Mexico,Wyoming,Arizona,Colorado,Idaho",	
+"Vermont":	"New Hampshire,New York,Massachusetts",	
+	"Virginia":	"North Carolina,Tennessee,West Virginia,Kentucky,Maryland",
+"Washington":	"Oregon,Idaho",	
+	"West Virginia": "Pennsylvania,Virginia,Kentucky,Maryland,Ohio",	
+	"Wisconsin":	"Michigan,Minnesota,Illinois,Iowa",	
+	"Wyoming":	"Nebraska,South Dakota,Utah,Colorado,Idaho,Montana"
 }
-"""
+
 
 car_pickle = open ("ati_dict.pkl", "rb")
 x = pickle.load(car_pickle)
@@ -161,10 +153,20 @@ for i in y.items():
 for i in y.items():
    y[i[0]]/=norm_factor_road
 
-print(x)
+#print(x)
 #print(y)
 states=list(us_state.values())
-a = np.zeros((50,50,2))
+a = np.zeros((50,50,3))
+
+inv_states = {v: k for k, v in us_state.items()}
+
+
+for v in y.items():
+   try:
+      for n in neighboring_dict[inv_states[v[0]]].split(","):
+         a[states.index(us_state[n])][states.index(v[0])][2]+=v[1]
+   except:
+      continue
 
 counter1=0
 counter2=0
@@ -181,5 +183,6 @@ while counter2<50:
          
    counter2+=1
 
-print(a[states.index("FL")])
+print(a[states.index("TN")])
+
 
